da9a49908ab213d16c096b51489b6b4db6bf31fc
Coding Nexus <info@webnexus.co.uk>
2023-01-08 16:55:29 +0000

    Completing the set task in the README.md file
    If working outside of the scope, I would have added a button to trigger a start, stop and restart function.

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-alert("Update the code in script.js");
+let isDragging = false;
+let startX, startY;
+
+document.addEventListener('mousedown', e => {
+  isDragging = true;
+  startX = e.pageX;
+  startY = e.pageY;
+});
+
+document.addEventListener('mousemove', e => {
+  e.preventDefault();
+  if (!isDragging) return;
+
+  const selection = document.getElementById('selection');
+  selection.style.left = Math.min(e.pageX, startX) + 'px';
+  selection.style.top = Math.min(e.pageY, startY) + 'px';
+  selection.style.width = Math.abs(e.pageX - startX) + 'px';
+  selection.style.height = Math.abs(e.pageY - startY) + 'px';
+});
+
+document.addEventListener('mouseup', e => {
+  isDragging = false;
+
+  const selectionRect = document.getElementById('selection').getBoundingClientRect();
+  let largestElement = null;
+  let largestArea = 0;
+  document.querySelectorAll('.min-h-full *').forEach(element => {
+    const rect = element.getBoundingClientRect();
+    if (rect.left >= selectionRect.left && rect.top >= selectionRect.top && rect.right <= selectionRect.right && rect.bottom <= selectionRect.bottom) {
+      const area = rect.width * rect.height;
+      if (area > largestArea) {
+        largestElement = element;
+        largestArea = area;
+      }
+    }
+  });
+
+  largestElement.style.border = '2px solid red';
+  largestElement.style.borderRadius = '5px';
+
+  document.getElementById('overlay').style.display = 'none';
+});
